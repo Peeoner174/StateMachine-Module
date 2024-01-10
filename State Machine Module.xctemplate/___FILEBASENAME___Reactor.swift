423126d597742989dc08ec91c912d497f12d0c37
Pavel Kochenda <peeoner174@gmail.com>
2024-01-11 01:23:09 +0500

    [add] NoRouterRepresentable Reactor

---
+++ State Machine Module.xctemplate/___FILEBASENAME___Reactor.swift
@@ -0,0 +1,50 @@
+//___FILEHEADER___
+
+import Combine
+
+final class ___VARIABLE_productName:identifier___Reactor: ReactorNoRouterRepresentable, SubscriptionStore {
+    // MARK: - Public properties
+
+    // Architecture Scene properties
+    @Published var state = State()
+    let actionSubject = PassthroughSubject<Action, Never>()
+    let interactor: ___VARIABLE_productName:identifier___Interactor
+
+    // MARK: - Init
+
+    init(interactor: RecommendationsInteractor) {
+        self.interactor = interactor
+        self.stateEmitter()
+    }
+
+    // MARK: - Public methods
+
+    func mutate(action: Action) -> AnyPublisher<Mutation, Never> {
+        switch action {}
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {}
+        return newState
+    }
+}
+
+extension ___VARIABLE_productName:identifier___Reactor {
+    // MARK: - Action
+
+    enum Action {}
+}
+
+extension ___VARIABLE_productName:identifier___Reactor {
+    // MARK: - Mutation
+
+    enum Mutation {}
+}
+
+extension RecommendationsReactor {
+    // MARK: - State
+
+    struct State {}
+}
