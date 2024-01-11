67d9871a9abc5522c6e616ee765d81be02a62082
Pavel Kochenda <peeoner174@gmail.com>
2024-01-11 12:57:46 +0500

    [add] With Router Template

---
+++ State Machine Module.xctemplate/StateMachine with Router/___FILEBASENAME___Reactor.swift
@@ -0,0 +1,52 @@
+//___FILEHEADER___
+
+import Combine
+
+final class ___VARIABLE_productName:identifier___Reactor: ReactorNoRouterRepresentable, SubscriptionStore {
+    // MARK: - Public properties
+
+    // Architecture Scene properties
+    @Published var state = State()
+    let actionSubject = PassthroughSubject<Action, Never>()
+    let router: ___VARIABLE_productName:identifier___Router
+    let interactor: ___VARIABLE_productName:identifier___Interactor
+
+    // MARK: - Init
+
+    init(interactor: RecommendationsInteractor, router: ___VARIABLE_productName:identifier___Router) {
+        self.router = router
+        self.interactor = interactor
+        self.stateEmitter()
+    }
+
+    // MARK: - Public methods
+
+    func mutate(action: Action) -> AnyPublisher<Mutation, Never> {
+        switch action {}
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {}
+        return newState
+    }
+}
+
+extension ___VARIABLE_productName:identifier___Reactor {
+    // MARK: - Action
+
+    enum Action {}
+}
+
+extension ___VARIABLE_productName:identifier___Reactor {
+    // MARK: - Mutation
+
+    enum Mutation {}
+}
+
+extension ___VARIABLE_productName:identifier___Reactor {
+    // MARK: - State
+
+    struct State {}
+}
---
+++ State Machine Module.xctemplate/StateMachine without Router/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,15 @@
+//___FILEHEADER___
+
+final class ___VARIABLE_productName:identifier___Interactor: InteractorRepresentable {
+    // MARK: - Public properties
+
+    var model: ModelRepresentable?
+    var service: ___VARIABLE_productName:identifier___Service?
+    var configurator: ___VARIABLE_productName:identifier___Configurator?
+
+    // MARK: - Init
+
+    init() {}
+
+    // MARK: Public methods
+}
